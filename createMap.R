697f5a8bddd4d12d5d97755da089114707200f59
Dusty <bsdpunk@gmail.com>
2017-12-08 15:02:01 -0600

    AddedMurderyBits

--- createMap.R
+++ createMap.R
@@ -1,6 +1,19 @@
+#Get Accidental Drug Deaths, and plot location on a map
 accDeathSed <- read.csv("accDeathsed.csv",stringsAsFactors = FALSE)
 accDeathMap <- get_map(location = "connecticut",maptype = "roadmap")
 accDeathMapGG <- ggmap(accDeathMap)
 accDeathMapGG + 
   geom_point(data = accDeathSed, aes(x = long, y = lat), color = "black", size = 3) +
   geom_point(data = accDeathSed, aes(x = long, y = lat), color = "red", size = 2)
+
+
+#Get Philadelphia Crime Map
+philCrimeData <- read.csv("~/Downloads/crime.csv",stringsAsFactors = FALSE)
+philCrimeMap <- get_map(location = "philadelphia",maptype = "roadmap")
+philCrimeMapGG <- ggmap(philCrimeMap)
+
+#Plot Murder points on map
+philCrimeMapGG + 
+  geom_point(data = philCrimeData[grep(pattern = "Homicide",philCrimeData$Text_General_Code),], aes(x = Lon, y = Lat), color = "black", size = 3) +
+  geom_point(data = philCrimeData[grep(pattern = "Homicide",philCrimeData$Text_General_Code),], aes(x = Lon, y = Lat), color = "red", size = 2)
+
