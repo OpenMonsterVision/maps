c9b8b0b0689926b462e51ff7723e5efff093757b
Dusty <bsdpunk@gmail.com>
2017-12-08 16:04:28 -0600

    Colors

--- createMap.R
+++ createMap.R
@@ -9,15 +9,19 @@ accDeathMapGG +
 
 #Get Philadelphia Crime Map
 philCrimeData <- read.csv("~/Downloads/crime.csv",stringsAsFactors = FALSE)
-philCrimeMap <- get_map(location = "philadelphia",maptype = "roadmap")
+# Get Philly Map, Zoom in slightly
+philCrimeMap <- get_map(location = "philadelphia",maptype = "roadmap", zoom = 12)
+#Create Map
 philCrimeMapGG <- ggmap(philCrimeMap)
-
-#Plot Murder points on map
+#Plot Dots on Map
 philCrimeMapGG + 
-  geom_point(data = philCrimeData[grep(pattern = "Homicide",philCrimeData$Text_General_Code),], aes(x = Lon, y = Lat), color = "black", size = 3) +
-  geom_point(data = philCrimeData[grep(pattern = "Homicide",philCrimeData$Text_General_Code),], aes(x = Lon, y = Lat), color = "red", size = 2) +
-#Add the rapey bits
-  geom_point(data = philCrimeData[which( philCrimeData$Text_General_Code == 'Rape' ),], aes(x = Lon, y = Lat), color = "black", size = 3) +
-  geom_point(data = philCrimeData[which( philCrimeData$Text_General_Code == 'Rape' ),], aes(x = Lon, y = Lat), color = "violet", size = 2)
-
+#Add Murdery Dots
+  #Very Transparent black dot, for darkening and border
+  geom_point(data = philCrimeData[grep(pattern = "Homicide",philCrimeData$Text_General_Code),], aes(x = Lon, y = Lat), color = "black", size = 1, alpha = 0.05) +
+  #Actual Murder Dot
+  geom_point(data = philCrimeData[grep(pattern = "Homicide",philCrimeData$Text_General_Code),], aes(x = Lon, y = Lat), color = "red", size = 0.5, alpha = 0.2) + 
+  #Very Transparent black dot, for darkening and border
+  geom_point(data = philCrimeData[which( philCrimeData$Text_General_Code == 'Rape' ),], aes(x = Lon, y = Lat), color = "black", size = 1, alpha = 0.05) +
+  #Rape Dot
+  geom_point(data = philCrimeData[which( philCrimeData$Text_General_Code == 'Rape' ),], aes(x = Lon, y = Lat), color = "green", size = 0.5, alpha = 0.2)
 
